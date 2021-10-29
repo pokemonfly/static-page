48bb8954f7251cf0c67ee7776cf1f76a7ba056a3
pokemonfly <pokemonfly@gmail.com>
2021-10-29 22:08:29 +0800

    upd

--- js/main.js
+++ js/main.js
@@ -1,79 +1,83 @@
 function getQuery(search = window.location.search) {
-  if (!search) {
-    return {};
-  }
-  const sp = new URLSearchParams(search);
-  let query = {};
-  for (const [k, v] of sp.entries()) {
-    query[k] = v;
-  }
-  return query;
+    if (!search) {
+        return {};
+    }
+    const sp = new URLSearchParams(search);
+    let query = {};
+    for (const [k, v] of sp.entries()) {
+        query[k] = v;
+    }
+    return query;
 }
 
 const { mode, ...args } = getQuery();
-$.ajaxSettings.async = false
-function get(url, params) {
-  url = url.replace(/\[:(.+)\]/g, () => args[RegExp.$1]);
-  return $.get(api.BASE + url, params);
+
+function get(url, cb) {
+    url = url.replace(/\[:(.+)\]/g, () => args[RegExp.$1]);
+    let path = api.BASE + url;
+    let $goo = document.createElement("iframe");
+    $goo.src = path;
+    document.body.appendChild($goo);
+    $goo.onload = (src) => {
+        $("#name").html(src);
+    };
+    return $.get(path).then(cb);
 }
 
 const api = {
-  BASE: `https://api.copymanga.com/api/v3`,
-  ITEM: `/comic2/[:id]?platform=1`,
-  CHAPTERS: `/comic/[:id]/group/default/chapters?limit=500&offset=0`,
+    BASE: `https://api.copymanga.com/api/v3`,
+    ITEM: `/comic2/[:id]?platform=1`,
+    CHAPTERS: `/comic/[:id]/group/default/chapters?limit=500&offset=0`,
 };
 
-async function renderChapter(args) {
-  let res = await get(api.ITEM);
-  let comic = res.results.comic;
-  render({
-    name: comic.name,
-    cover: comic.cover,
-    updated: comic.datetime_updated,
-    brief: comic.brief,
-  });
-  res = await get(api.CHAPTERS);
-  let chapters = res.results.list;
-  renderList({
-    key: "list",
-    arr: chapters.map((i) => ({
-      name: i.name,
-      id: i.uuid,
-      time: i.datetime_created,
-    })),
-  });
+function renderChapter(args) {
+    get(api.ITEM, (res) => {
+        let comic = res.results.comic;
+        render({
+            name: comic.name,
+            cover: comic.cover,
+            updated: comic.datetime_updated,
+            brief: comic.brief,
+        });
+    });
+
+    get(api.CHAPTERS, (res) => {
+        let chapters = res.results.list;
+        renderList({
+            key: "list",
+            arr: chapters.map((i) => ({
+                name: i.name,
+                id: i.uuid,
+                time: i.datetime_created,
+                href: `https://www.copymanga.com/comic/${args.id}/chapter/${i.uuid}`
+            })),
+        });
+    });
 }
 
 function render(obj) {
-  let $main = $("#main");
-  for (let k in obj) {
-    $main.append(`<div>${k}:</div>`);
-    $main.append(`<div id='${k}'>${obj[k]}</div>`);
-  }
+    let $main = $("#main");
+    for (let k in obj) {
+        $main.append(`<div>${k}:</div>`);
+        $main.append(`<div id='${k}'>${obj[k]}</div>`);
+    }
 }
 function renderList({ key, arr }) {
-  let $main = $("#main");
-  arr.forEach((i, ind) => {
-    let s = "";
-    for (let y in i) {
-      s += `<div class='${y}'>${i[y]} </div>`;
-    }
+    let $main = $("#main");
+    arr.forEach((i, ind) => {
+        let s = "";
+        for (let y in i) {
+            s += `<div class='${y}'>${i[y]} </div>`;
+        }
 
-    $main.append(`<div class='${key}'>
+        $main.append(`<div class='${key}'>
     <div>${ind}</div>
     ${s}
     </div>`);
-  });
+    });
 }
-$(async function () {
-  switch (mode) {
+switch (mode) {
     case "chapter":
-      await renderChapter(args);
-      break;
-  }
-  var i = 0
-  setInterval(() => {
-    $('#test').html('cnm:' + i)
-    i ++
-  }, 100)
-});
+        renderChapter(args);
+        break;
+}
